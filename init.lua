4ed84f6886fc98d813cbdd0a5b0efb9c7edc44fe
Jiraffe7 <twajxjiraffe@gmail.com>
2026-04-05 10:35:19 -0700

    Remove mason-tool-installer

--- init.lua
+++ init.lua
@@ -586,7 +586,6 @@ require('lazy').setup({
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'mason-org/mason.nvim', opts = {} },
       'mason-org/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
